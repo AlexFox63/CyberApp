d95f2742e33ddbb8f9762be866e1186e7da2084c
alexFox63 <33263736+alexFox63@users.noreply.github.com>
2018-12-09 14:41:47 +0400

    V 0.0.1

--- app/src/main/java/com/example/fokin/recyclerviewquestion/MainActivity.java
+++ app/src/main/java/com/example/fokin/recyclerviewquestion/MainActivity.java
@@ -1,34 +1,83 @@
 package com.example.fokin.recyclerviewquestion;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Question.Answer> answers = new ArrayList<>();
+    private RelativeLayout mRelativeLayout;
+    private Button btnNext;
+    private Boolean trying = false;
+    private DrawerLayout mDrawerLayout;
+    private int count = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setInitialData();
-        RecyclerView recyclerView = findViewById(R.id.list);
-        // создаем адаптер
-        DataAdapter adapter = new DataAdapter(this, answers);
-        // устанавливаем для списка адаптер
-        recyclerView.setAdapter(adapter);
+        btnNext = findViewById(R.id.btnNext);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mRelativeLayout = findViewById(R.id.content_frame);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        FragmentQuestion fragmentQuestion = new FragmentQuestion();
+        FragmentResult fragmentResult = new FragmentResult();
+        /**Счетчик вопросов и переключение на фрагмент с результатом теста**/
+        if(count < 0) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.frame_fragment, fragmentQuestion);
+            fragmentTransaction.commit();
+        }
+        else{
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.frame_fragment, fragmentResult);
+            fragmentTransaction.commit();
+            if(trying){
+                mRelativeLayout.setBackground(getDrawable(R.drawable.suhgood));
+            }
+            else{
+                mRelativeLayout.setBackground(getDrawable(R.drawable.notgood));
+            }
+            btnNext.setText("Пройти тест заново");
+
+        }
+
     }
 
-    private void setInitialData(){
 
-        answers.add(new Question.Answer("Протокол TCP/IP"));
-        answers.add(new Question.Answer("Второй ответ"));
-        answers.add(new Question.Answer("Третий"));
-        answers.add(new Question.Answer("Четвертый"));
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
--- app/src/main/java/com/example/fokin/recyclerviewquestion/StartActivity.java
+++ app/src/main/java/com/example/fokin/recyclerviewquestion/StartActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -23,4 +27,32 @@ public class StartActivity extends AppCompatActivity {
                 }
         );
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.app_bar_about:
+                Toast.makeText(getApplicationContext(), "Вы выбрали офф сайт", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.app_bar_pay:
+                Toast.makeText(getApplicationContext(), "Вы заплатили налоги", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
